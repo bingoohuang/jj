8543bee4fdca3796758b1bbe63dfa698be5bcbb0
bingoohuang <bingoo.huang@gmail.com>
2021-06-07 14:44:25 +0800

    refactor

--- gen.go
+++ gen.go
@@ -22,82 +22,11 @@ var DefaultGen = NewGenContext()
 func init() {
 	DefaultGen.RegisterFn("random", Random)
 	DefaultGen.RegisterFn("random_int", RandomInt)
-	DefaultGen.RegisterFn("random_bool", RandomBool)
+	DefaultGen.RegisterFn("random_bool", func(_ string) interface{} { return randx.Bool() })
 	DefaultGen.RegisterFn("random_time", RandomTime)
-	DefaultGen.RegisterFn("objectId", ObjectId)
+	DefaultGen.RegisterFn("objectId", func(string) interface{} { return NewObjectID().Hex() })
 	DefaultGen.RegisterFn("regex", Regex)
-	DefaultGen.RegisterFn("uuid", Uuid)
-}
-
-func Uuid(_ string) interface{} {
-	return NewUUID().String()
-}
-
-// NewUUID creates a new random UUID or panics.
-func NewUUID() UUID {
-	return MustNewUUID(NewRandomUUID())
-}
-
-// MustNewUUID returns uuid if err is nil and panics otherwise.
-func MustNewUUID(uuid UUID, err error) UUID {
-	if err != nil {
-		panic(err)
-	}
-	return uuid
-}
-
-// String returns the string form of uuid, xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
-// , or "" if uuid is invalid.
-func (uuid UUID) String() string {
-	var buf [36]byte
-	encodeHex(buf[:], uuid)
-	return string(buf[:])
-}
-
-func encodeHex(dst []byte, uuid UUID) {
-	hex.Encode(dst, uuid[:4])
-	dst[8] = '-'
-	hex.Encode(dst[9:13], uuid[4:6])
-	dst[13] = '-'
-	hex.Encode(dst[14:18], uuid[6:8])
-	dst[18] = '-'
-	hex.Encode(dst[19:23], uuid[8:10])
-	dst[23] = '-'
-	hex.Encode(dst[24:], uuid[10:])
-}
-
-var rander = rand.Reader // random function
-var Nil UUID             // empty UUID, all zeros
-
-// A UUID is a 128 bit (16 byte) Universal Unique IDentifier as defined in RFC 4122.
-type UUID [16]byte
-
-// NewRandomUUID returns a Random (Version 4) UUID.
-//
-// The strength of the UUIDs is based on the strength of the crypto/rand
-// package.
-//
-// A note about uniqueness derived from the UUID Wikipedia entry:
-//
-//  Randomly generated UUIDs have 122 random bits.  One's annual risk of being
-//  hit by a meteorite is estimated to be one chance in 17 billion, that
-//  means the probability is about 0.00000000006 (6 × 10−11),
-//  equivalent to the odds of creating a few tens of trillions of UUIDs in a
-//  year and having one duplicate.
-func NewRandomUUID() (UUID, error) {
-	return NewRandomUUIDFromReader(rander)
-}
-
-// NewRandomUUIDFromReader returns a UUID based on bytes read from a given io.Reader.
-func NewRandomUUIDFromReader(r io.Reader) (UUID, error) {
-	var uuid UUID
-	_, err := io.ReadFull(r, uuid[:])
-	if err != nil {
-		return Nil, err
-	}
-	uuid[6] = (uuid[6] & 0x0f) | 0x40 // Version 4
-	uuid[8] = (uuid[8] & 0x3f) | 0x80 // Variant is 10
-	return uuid, nil
+	DefaultGen.RegisterFn("uuid", func(_ string) interface{} { return NewUUID().String() })
 }
 
 type SubstitutionFnMap map[string]SubstitutionFn
@@ -561,10 +490,6 @@ func filter(pp []string, s string) (filtered []string, found bool) {
 	return
 }
 
-func RandomBool(_ string) interface{} {
-	return randx.Bool()
-}
-
 func RandomInt(args string) interface{} {
 	if args == "" {
 		return randx.Int64()
@@ -646,8 +571,6 @@ type ObjectID [12]byte
 var objectIDCounter = readRandomUint32()
 var processUnique = processUniqueBytes()
 
-func ObjectId(string) interface{} { return NewObjectID().Hex() }
-
 // NewObjectID generates a new ObjectID.
 func NewObjectID() ObjectID {
 	return NewObjectIDFromTimestamp(time.Now())
@@ -699,4 +622,71 @@ func putUint24(b []byte, v uint32) {
 	b[0] = byte(v >> 16)
 	b[1] = byte(v >> 8)
 	b[2] = byte(v)
+}
+
+// NewUUID creates a new random UUID or panics.
+func NewUUID() UUID {
+	return MustNewUUID(NewRandomUUID())
+}
+
+// MustNewUUID returns uuid if err is nil and panics otherwise.
+func MustNewUUID(uuid UUID, err error) UUID {
+	if err != nil {
+		panic(err)
+	}
+	return uuid
+}
+
+// String returns the string form of uuid, xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+// , or "" if uuid is invalid.
+func (uuid UUID) String() string {
+	var buf [36]byte
+	encodeHex(buf[:], uuid)
+	return string(buf[:])
+}
+
+func encodeHex(dst []byte, uuid UUID) {
+	hex.Encode(dst, uuid[:4])
+	dst[8] = '-'
+	hex.Encode(dst[9:13], uuid[4:6])
+	dst[13] = '-'
+	hex.Encode(dst[14:18], uuid[6:8])
+	dst[18] = '-'
+	hex.Encode(dst[19:23], uuid[8:10])
+	dst[23] = '-'
+	hex.Encode(dst[24:], uuid[10:])
+}
+
+var rander = rand.Reader // random function
+var Nil UUID             // empty UUID, all zeros
+
+// A UUID is a 128 bit (16 byte) Universal Unique IDentifier as defined in RFC 4122.
+type UUID [16]byte
+
+// NewRandomUUID returns a Random (Version 4) UUID.
+//
+// The strength of the UUIDs is based on the strength of the crypto/rand
+// package.
+//
+// A note about uniqueness derived from the UUID Wikipedia entry:
+//
+//  Randomly generated UUIDs have 122 random bits.  One's annual risk of being
+//  hit by a meteorite is estimated to be one chance in 17 billion, that
+//  means the probability is about 0.00000000006 (6 × 10−11),
+//  equivalent to the odds of creating a few tens of trillions of UUIDs in a
+//  year and having one duplicate.
+func NewRandomUUID() (UUID, error) {
+	return NewRandomUUIDFromReader(rander)
+}
+
+// NewRandomUUIDFromReader returns a UUID based on bytes read from a given io.Reader.
+func NewRandomUUIDFromReader(r io.Reader) (UUID, error) {
+	var uuid UUID
+	_, err := io.ReadFull(r, uuid[:])
+	if err != nil {
+		return Nil, err
+	}
+	uuid[6] = (uuid[6] & 0x0f) | 0x40 // Version 4
+	uuid[8] = (uuid[8] & 0x3f) | 0x80 // Variant is 10
+	return uuid, nil
 }
